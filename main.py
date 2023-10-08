22c5408ed3b9e38818ccc04dbea686feafca2278
hayatroid <hayatroid@gmail.com>
2023-10-08 17:47:21 +0900

    GitHub Actions テスト

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ res = requests.get(url)
 soup = BeautifulSoup(res.text, "html.parser")
 price = soup.select(".YMlKec.fxKbKc")[0].contents[0]
 
-message = f"現在の日経平均株価は {price} 円です"
+message = f"【テスト】現在の日経平均株価は {price} 円です"
 webhook_id = os.environ.get("WEBHOOK_ID")
 subprocess.run(f'curl -X POST -H "Content-Type: text/plain; charset=utf-8" -d "{message}" https://q.trap.jp/api/v3/webhooks/{webhook_id}', shell=True)
