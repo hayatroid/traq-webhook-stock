9d2fb2103f906d1231874e90d9e11341362ee9b6
hayatroid <hayatroid@gmail.com>
2023-10-08 17:25:14 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.google.com/finance/quote/NI225:INDEXNIKKEI"
+res = requests.get(url)
+soup = BeautifulSoup(res.text, "html.parser")
+price = soup.select(".YMlKec.fxKbKc")[0].contents[0]
+
+message = f"現在の日経平均株価は {price} 円です"
+webhook_id = os.environ.get("WEBHOOK_ID")
+subprocess.run(f'curl -X POST -H "Content-Type: text/plain; charset=utf-8" -d "{message}" https://q.trap.jp/api/v3/webhooks/{webhook_id}', shell=True)
