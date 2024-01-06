c299b212d018326badfdd8f200ee76b634d896fb
hayatroid <hayatroid@gmail.com>
2024-01-06 18:45:35 +0900

    指数名をスタンプに

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from playwright.sync_api import sync_playwright
 import requests
 from zoneinfo import ZoneInfo
 
-message = f"""為替と株の値動きです ({datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%m/%d %H:%M")})
-|指数名|現在値|前日比|
-|-|-|-|
+now = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y/%m/%d %H:%M")
+message = f"""為替と株の値動きです ({now})
+||現在値$\hspace{{5px}}$|前日比$\hspace{{8px}}$|
+|:-:|-:|-:|
 """
 
 html = ""
@@ -19,9 +20,9 @@ with sync_playwright() as playwright:
     html = page.content()
 
 soup = BeautifulSoup(html, "html.parser")
+names = [":nikkei_heikin:", ":topix:", ":growth250:", ":sp500:", ":usd_jpy:", ":eur_jpy:"]
 codes = ["NN0", "NN2", "NN4", "NN9", "NN14", "NN15"]
-for code in codes:
-    name = soup.select(f"#{code} .ttN")[0].contents[0].text.strip()
+for name, code in zip(names, codes):
     price = soup.select(f"#{code} .怩侖鍠爲發隲蛞筵l穡倥")[0].contents[0].text
     ratio = soup.select(f"#{code} .亠搨ｰ臻l佩韆淅嚏l仄淪ｪN")[0].contents[0].text
     if code in ["NN14", "NN15"]:
